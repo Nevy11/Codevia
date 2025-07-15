3f7b4d7274077f0a51d19a451beb6a72d53b1b7f
nevy11 <smongare2003@gmail.com>
2025-07-15 18:54:24 +0300

    chore: update Angular dependencies to version 20.1.0 and TypeScript to 5.8.3
    - Upgraded all Angular packages from version 19.2.x to 20.1.0.
    - Updated TypeScript version from 5.5.2 to 5.8.3.
    - Removed SignupComponent import from AppComponent as it was not used.
    - Changed import source for provideServerRendering from '@angular/platform-server' to '@angular/ssr'.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SignupComponent } from './signup/signup.component';
 
 @Component({
-    selector: 'nevy11-root',
-    imports: [RouterOutlet, SignupComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'nevy11-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'E-learning-platform';
