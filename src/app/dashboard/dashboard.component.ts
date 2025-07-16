c9f7de5a2852ebb278d50c3d09cc2e8727953baa
nevy11 <smongare2003@gmail.com>
2025-07-16 17:54:05 +0300

    feat: update card titles in DashboardComponent for clarity

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,17 +9,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
-    selector: 'nevy11-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrl: './dashboard.component.scss',
-    imports: [
-        AsyncPipe,
-        MatGridListModule,
-        MatMenuModule,
-        MatIconModule,
-        MatButtonModule,
-        MatCardModule
-    ]
+  selector: 'nevy11-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss',
+  imports: [
+    AsyncPipe,
+    MatGridListModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
 })
 export class DashboardComponent {
   private breakpointObserver = inject(BreakpointObserver);
@@ -29,18 +29,18 @@ export class DashboardComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          { title: 'Learn', cols: 1, rows: 1 },
+          { title: 'Course', cols: 1, rows: 1 },
+          { title: 'Stats', cols: 1, rows: 1 },
+          { title: 'Setting', cols: 1, rows: 1 },
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        { title: 'Learn', cols: 2, rows: 1 },
+        { title: 'Courses', cols: 1, rows: 1 },
+        { title: 'Statistics', cols: 1, rows: 2 },
+        { title: 'Settings', cols: 1, rows: 1 },
       ];
     })
   );
