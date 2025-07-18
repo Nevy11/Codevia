73d8b104735e69450486efaeead63c559cfb6751
nevy11 <smongare2003@gmail.com>
2025-07-18 18:32:48 +0300

    Add TypeScript image to home page assets

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  inject,
+  OnInit,
+  OnDestroy,
+  PLATFORM_ID,
+  Inject,
+} from '@angular/core';
+import { map } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'nevy11-landing-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit, OnDestroy {
+  private breakpointObserver = inject(BreakpointObserver);
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  // rendering different images
+  images = [
+    '/home_page/angular.jpeg',
+    '/home_page/cool.jpeg',
+    '/home_page/download.jpeg',
+    '/home_page/typescript.jpeg',
+  ];
+  currentImageIndex = 0;
+  private intervalId: any;
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Start the interval only if the platform is a browser
+      this.intervalId = setInterval(() => {
+        this.currentImageIndex =
+          (this.currentImageIndex + 1) % this.images.length;
+      }, 5000);
+    }
+  }
+  ngOnDestroy() {
+    // Clear the interval when the component is destroyed
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+      } else {
+        // tablet potrait view
+        const isTabletPotrait = this.breakpointObserver.isMatched(
+          Breakpoints.TabletPortrait
+        );
+        if (isTabletPotrait) {
+        } else {
+          // small laptop view
+          const isSmallLaptop = this.breakpointObserver.isMatched(
+            '(min-width: 840px) and (max-width: 1366px)'
+          );
+          if (isSmallLaptop) {
+          } else {
+            // Large laptop view
+          }
+        }
+      }
+    })
+  );
 }
