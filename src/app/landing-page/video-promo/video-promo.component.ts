be3781949d884dbea7210064b0ce2dbfcb6a6dfb
nevy11 <smongare2003@gmail.com>
2025-07-22 17:51:03 +0300

    update top level in mobile device landing page

--- src/app/landing-page/video-promo/video-promo.component.ts
+++ src/app/landing-page/video-promo/video-promo.component.ts
@@ -4,28 +4,24 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { VideoDialogComponent } from './video-dialog/video-dialog.component';
 import { NgOptimizedImage } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'nevy11-video-promo',
-  imports: [MatButtonModule, MatDialogModule, MatIconModule, NgOptimizedImage],
+  imports: [
+    MatButtonModule,
+    MatDialogModule,
+    MatIconModule,
+    NgOptimizedImage,
+    AsyncPipe,
+  ],
   templateUrl: './video-promo.component.html',
   styleUrl: './video-promo.component.scss',
 })
 export class VideoPromoComponent {
-  // showVideo = false;
-  // showCTA = false;
-
-  // playVideo() {
-  //   this.showVideo = true;
-  //   setTimeout(() => {
-  //     this.showCTA = true;
-  //   }, 5000); // Show CTA after 5 seconds
-  // }
-
-  // closeVideo() {
-  //   this.showVideo = false;
-  //   this.showCTA = false;
-  // }
+  private breakpointObserver = inject(BreakpointObserver);
   private dialog = inject(MatDialog);
   openVideo(): void {
     console.log('Opening video dialog');
@@ -35,4 +31,59 @@ export class VideoPromoComponent {
       panelClass: 'video-dialog',
     });
   }
+
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          {
+            mobile_device: true,
+            isTabletPotrait: false,
+            isSmallLaptop: false,
+            isLargeLaptop: false,
+          },
+        ];
+      } else {
+        // tablet potrait view
+        const isTabletPotrait = this.breakpointObserver.isMatched(
+          Breakpoints.TabletPortrait
+        );
+        if (isTabletPotrait) {
+          return [
+            {
+              mobile_device: false,
+              isTabletPotrait: true,
+              isSmallLaptop: false,
+              isLargeLaptop: false,
+            },
+          ];
+        } else {
+          // small laptop view
+          const isSmallLaptop = this.breakpointObserver.isMatched(
+            '(min-width: 840px) and (max-width: 1366px)'
+          );
+          if (isSmallLaptop) {
+            return [
+              {
+                mobile_device: false,
+                isTabletPotrait: false,
+                isSmallLaptop: true,
+                isLargeLaptop: false,
+              },
+            ];
+          } else {
+            return [
+              {
+                mobile_device: false,
+                isTabletPotrait: false,
+                isSmallLaptop: false,
+                isLargeLaptop: true,
+              },
+            ];
+            // Large laptop view
+          }
+        }
+      }
+    })
+  );
 }
