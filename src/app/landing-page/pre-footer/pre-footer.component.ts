df8646f6718567fa708b4d62c6c828085cb89231
nevy11 <smongare2003@gmail.com>
2025-07-24 15:28:50 +0300

    update default colors in the prefooter

--- src/app/landing-page/pre-footer/pre-footer.component.ts
+++ src/app/landing-page/pre-footer/pre-footer.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'nevy11-pre-footer',
@@ -8,4 +9,15 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './pre-footer.component.html',
   styleUrl: './pre-footer.component.scss',
 })
-export class PreFooterComponent {}
+export class PreFooterComponent {
+  router = inject(Router);
+
+  login() {
+    console.log('Login button clicked');
+    this.router.navigate(['login']);
+  }
+  signUp() {
+    console.log('Sign Up button clicked');
+    this.router.navigate(['signup']);
+  }
+}
