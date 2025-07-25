3971a7ec0991b533f68c0a05fb100c759cff4c91
nevy11 <smongare2003@gmail.com>
2025-07-25 17:15:50 +0300

    update dark and light toggle mode

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,6 +6,8 @@ import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { NgOptimizedImage } from '@angular/common';
+import { ThemeService } from '../theme.service';
+import { MatMenuModule } from '@angular/material/menu';
 @Component({
   selector: 'nevy11-layout',
   imports: [
@@ -16,10 +18,16 @@ import { NgOptimizedImage } from '@angular/common';
     MatListModule,
     RouterModule,
     NgOptimizedImage,
+    MatMenuModule,
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
   username: string = 'User'; // Placeholder for username
+  themeService = inject(ThemeService);
+  ngOnInit(): void {
+    // this.themeService.loadTheme();
+    this.themeService.setTheme('violet-theme');
+  }
 }
--- src/app/theme.service.ts
+++ src/app/theme.service.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class ThemeService {
-  
+  private isDark = false;
+
+  setTheme(themeName: string) {
+    document.body.classList.remove('violet-theme', 'magneta-theme');
+    if (themeName) {
+      document.body.classList.add(themeName);
+    }
+    localStorage.setItem('theme', themeName);
+  }
+
+  toggleDarkMode() {
+    this.isDark = !this.isDark;
+    if (this.isDark) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
+    localStorage.setItem('darkMode', this.isDark.toString());
+  }
+
+  loadTheme() {
+    const savedTheme = localStorage.getItem('theme') || '';
+    const darkMode = localStorage.getItem('darkMode') === 'true';
+
+    if (savedTheme) {
+      document.body.classList.add(savedTheme);
+    }
+    if (darkMode) {
+      document.body.classList.add('dark-theme');
+      this.isDark = true;
+    }
+  }
 }
