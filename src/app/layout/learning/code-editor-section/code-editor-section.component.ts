50ec1e2d5e6ba5fa9771e61edaae31b8a3087d6b
nevy11 <smongare2003@gmail.com>
2025-07-27 23:10:41 +0300

    feat: enhance code editor section with download, reset, and run functionalities

--- src/app/layout/learning/code-editor-section/code-editor-section.component.ts
+++ src/app/layout/learning/code-editor-section/code-editor-section.component.ts
@@ -2,17 +2,24 @@ import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 import { ThemeChangeService } from '../../../theme-change.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'nevy11-code-editor-section',
-  imports: [FormsModule, MonacoEditorModule],
+  imports: [
+    FormsModule,
+    MonacoEditorModule,
+    MatButtonModule,
+    MatSnackBarModule,
+  ],
   templateUrl: './code-editor-section.component.html',
   styleUrl: './code-editor-section.component.scss',
 })
 export class CodeEditorSectionComponent implements OnInit {
   code: string = `// Write your code here`;
   themechangeService = inject(ThemeChangeService);
-
+  matsnackbar = inject(MatSnackBar);
   editorOptions = {
     theme: 'vs-dark',
     language: 'javascript',
@@ -26,6 +33,19 @@ export class CodeEditorSectionComponent implements OnInit {
         ...this.editorOptions,
         theme: theme === 'dark' ? 'vs-dark' : 'vs-light',
       };
+    });
+  }
+  runCode() {
+    this.matsnackbar.open('Code executed successfully!', 'Close', {
+      duration: 2000,
+    });
+  }
+  resetCode() {
+    this.code = `// Write your code here`;
+  }
+  downloadCode() {
+    this.matsnackbar.open('Code downloaded successfully!', 'Close', {
+      duration: 2000,
     });
   }
 }
