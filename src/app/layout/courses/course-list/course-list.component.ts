9b7486535af9074824a52d79c8f123624552c9cc
nevy11 <smongare2003@gmail.com>
2025-07-28 16:37:31 +0300

    add course-list component

---
+++ src/app/layout/courses/course-list/course-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nevy11-course-list',
+  imports: [],
+  templateUrl: './course-list.component.html',
+  styleUrl: './course-list.component.scss'
+})
+export class CourseListComponent {
+
+}
