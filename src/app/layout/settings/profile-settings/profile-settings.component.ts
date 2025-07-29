864624c0d9ff9dbf96c88cf4f1be864e154c543a
nevy11 <smongare2003@gmail.com>
2025-07-29 19:42:59 +0300

    feat: implement profile settings with avatar upload and form fields

--- src/app/layout/settings/profile-settings/profile-settings.component.ts
+++ src/app/layout/settings/profile-settings/profile-settings.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'nevy11-profile-settings',
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, FormsModule, MatInputModule],
   templateUrl: './profile-settings.component.html',
-  styleUrl: './profile-settings.component.scss'
+  styleUrl: './profile-settings.component.scss',
 })
-export class ProfileSettingsComponent {
+export class ProfileSettingsComponent implements OnInit {
+  name = '';
+  email = '';
+  bio = '';
+  avatarUrl = '/about/laptop.jpg';
+  ngOnInit(): void {
+    this.loadProfile();
+  }
 
+  loadProfile() {
+    this.name = localStorage.getItem('profile_name') || '';
+    this.email = localStorage.getItem('profile_email') || '';
+    this.bio = localStorage.getItem('profile_bio') || '';
+    this.avatarUrl =
+      localStorage.getItem('profile_avatar') || '/about/laptop.jpg';
+  }
+
+  saveProfile() {
+    localStorage.setItem('profile_name', this.name);
+    localStorage.setItem('profile_email', this.email);
+    localStorage.setItem('profile_bio', this.bio);
+    localStorage.setItem('profile_avatar', this.avatarUrl);
+  }
+  onFileSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (!file) return;
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.avatarUrl = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
 }
--- src/app/layout/settings/settings.component.ts
+++ src/app/layout/settings/settings.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { PreferencesSettingsComponent } from './preferences-settings/preferences-settings.component';
 import { MatTabsModule } from '@angular/material/tabs';
+import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
 @Component({
   selector: 'nevy11-settings',
-  imports: [PreferencesSettingsComponent, MatTabsModule],
+  imports: [
+    PreferencesSettingsComponent,
+    MatTabsModule,
+    ProfileSettingsComponent,
+  ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss',
 })
