e3aa486ba26a868712148408bb26e9d9a36ff9a6
nevy11 <smongare2003@gmail.com>
2025-07-29 16:57:39 +0300

    remove custom ignore file

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'nevy11-home',
@@ -11,4 +12,8 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class HomeComponent {
   username = 'User'; // Placeholder for username
+  router = inject(Router);
+  ToLearning() {
+    this.router.navigate(['/layout/learning']);
+  }
 }
