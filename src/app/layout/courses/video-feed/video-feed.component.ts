09ae623a55c1f01a1efa5b3c98ff86964ec88d81
nevy11 <smongare2003@gmail.com>
2025-07-30 20:01:22 +0300

    feat: integrate YouTube API for dynamic video loading in VideoFeedComponent

--- src/app/layout/courses/video-feed/video-feed.component.ts
+++ src/app/layout/courses/video-feed/video-feed.component.ts
@@ -1,68 +1,31 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Video } from './video';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { YoutubeService } from '../../youtube.service';
 
 @Component({
   selector: 'nevy11-video-feed',
+  standalone: true,
   imports: [CommonModule, MatIconModule, MatButtonModule, MatCardModule],
   templateUrl: './video-feed.component.html',
   styleUrl: './video-feed.component.scss',
 })
-export class VideoFeedComponent {
+export class VideoFeedComponent implements OnInit {
   private router = inject(Router);
-  videos: Video[] = [
-    {
-      id: 'rQ_J9WH6CGk',
-      title: 'Sample Video 1',
-      thumbnailUrl: 'https://img.youtube.com/vi/rQ_J9WH6CGk/0.jpg',
-    },
-    {
-      id: 'rfscVS0vtbw',
-      title: 'Sample Video 2',
-      thumbnailUrl: 'https://img.youtube.com/vi/rfscVS0vtbw/0.jpg',
-    },
-    {
-      id: 'PkZNo7MFNFg',
-      title: 'Sample Video 3',
-      thumbnailUrl: 'https://img.youtube.com/vi/PkZNo7MFNFg/0.jpg',
-    },
-    {
-      id: 'rQ_J9WH6CGk',
-      title: 'Sample Video 1',
-      thumbnailUrl: 'https://img.youtube.com/vi/rQ_J9WH6CGk/0.jpg',
-    },
-    {
-      id: 'rfscVS0vtbw',
-      title: 'Sample Video 2',
-      thumbnailUrl: 'https://img.youtube.com/vi/rfscVS0vtbw/0.jpg',
-    },
-    {
-      id: 'PkZNo7MFNFg',
-      title: 'Sample Video 3',
-      thumbnailUrl: 'https://img.youtube.com/vi/PkZNo7MFNFg/0.jpg',
-    },
-    {
-      id: 'rQ_J9WH6CGk',
-      title: 'Sample Video 1',
-      thumbnailUrl: 'https://img.youtube.com/vi/rQ_J9WH6CGk/0.jpg',
-    },
-    {
-      id: 'rfscVS0vtbw',
-      title: 'Sample Video 2',
-      thumbnailUrl: 'https://img.youtube.com/vi/rfscVS0vtbw/0.jpg',
-    },
-    {
-      id: 'PkZNo7MFNFg',
-      title: 'Sample Video 3',
-      thumbnailUrl: 'https://img.youtube.com/vi/PkZNo7MFNFg/0.jpg',
-    },
-  ];
-  playVideo(video: Video) {
-    // Navigate to learning section, passing video ID as parameter
+  private youtube = inject(YoutubeService);
+
+  videos: any[] = [];
+
+  ngOnInit() {
+    this.youtube.getVideos('full course').subscribe((data) => {
+      this.videos = data;
+    });
+  }
+
+  playVideo(video: any) {
     this.router.navigate(['/layout/learning'], {
       queryParams: { video: video.id },
     });
--- src/app/layout/youtube.service.ts
+++ src/app/layout/youtube.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -21,5 +22,20 @@ export class YoutubeService {
         type: 'video',
       },
     });
+  }
+
+  getVideos(query: string, maxResults: number = 50): Observable<any[]> {
+    const url = `${this.apiUrl}?part=snippet&type=video&q=${encodeURIComponent(
+      query
+    )}&maxResults=${maxResults}&key=${this.apiKey}`;
+    return this.http.get<any>(url).pipe(
+      map((res) =>
+        res.items.map((item: any) => ({
+          id: item.id.videoId,
+          title: item.snippet.title,
+          thumbnailUrl: item.snippet.thumbnails.high.url,
+        }))
+      )
+    );
   }
 }
