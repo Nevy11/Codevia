7d40558dcb84230772cfe3214e0e11d0c54a7fdf
nevy11 <smongare2003@gmail.com>
2025-08-05 19:05:14 +0300

    card to choose between mobile and non-mobile view

--- src/app/layout/settings/profile-setup-stepper/profile-setup-stepper.component.ts
+++ src/app/layout/settings/profile-setup-stepper/profile-setup-stepper.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MobileStepperComponent } from './mobile-stepper/mobile-stepper.component';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { NonMobileStepperComponent } from './non-mobile-stepper/non-mobile-stepper.component';
 
 @Component({
   selector: 'nevy11-profile-setup-stepper',
-  imports: [MobileStepperComponent],
+  imports: [MobileStepperComponent, AsyncPipe, NonMobileStepperComponent],
   templateUrl: './profile-setup-stepper.component.html',
   styleUrl: './profile-setup-stepper.component.scss',
 })
-export class ProfileSetupStepperComponent {}
+export class ProfileSetupStepperComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          {
+            mobile_device: true,
+          },
+        ];
+      } else {
+        // tablet potrait view
+        const isTabletPotrait = this.breakpointObserver.isMatched(
+          Breakpoints.TabletPortrait
+        );
+        if (isTabletPotrait) {
+          return [
+            {
+              mobile_device: false,
+            },
+          ];
+        } else {
+          // small laptop view
+          const isSmallLaptop = this.breakpointObserver.isMatched(
+            '(min-width: 840px) and (max-width: 1366px)'
+          );
+          if (isSmallLaptop) {
+            return [
+              {
+                mobile_device: false,
+              },
+            ];
+          } else {
+            return [
+              {
+                mobile_device: false,
+              },
+            ];
+            // Large laptop view
+          }
+        }
+      }
+    })
+  );
+}
