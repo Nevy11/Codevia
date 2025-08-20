8c554f89fd51c421c4bedf8122f8da03c0388af4
nevy11 <smongare2004@gmail.com>
2025-08-20 19:05:53 +0300

    fix course issue error

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,11 +1,11 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { Breakpoints } from '@angular/cdk/layout';
-import { map } from 'rxjs';
+import { from, map, of, switchMap } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { SupabaseClientService } from '../../supabase-client.service';
 import { Profile } from '../settings/profile-settings/profile';
@@ -25,6 +25,14 @@ export class HomeComponent implements OnInit {
   private supabaseService = inject(SupabaseClientService);
   user_id: string | null = null;
   stats: Stats | null = null;
+  userId$ = from(this.supabaseService.getCurrentUserId());
+
+  stats$ = this.userId$.pipe(
+    switchMap((userId) => {
+      if (!userId) return of(null);
+      return from(this.supabaseService.getCourseStats(userId));
+    })
+  );
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
@@ -75,12 +83,18 @@ export class HomeComponent implements OnInit {
     this.profile = await this.supabaseService.getProfile();
     if (this.profile) {
       this.username = this.profile.name;
-      console.log('username: ', this.profile.name);
     }
+
     this.user_id = await this.supabaseService.getCurrentUserId();
     if (this.user_id) {
-      this.stats = await this.supabaseService.getCourseStats(this.user_id);
-      console.log('stats: ', this.stats);
+      // create observable AFTER we know the user_id
+      this.stats$ = from(this.supabaseService.getCourseStats(this.user_id));
+
+      // if you still want the console log:
+      this.stats$.subscribe((stat) => {
+        this.stats = stat;
+        console.log('stats from observable:', stat);
+      });
     }
   }
 }
