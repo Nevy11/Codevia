4f3eb86b5ac0dfa69d8ed4d09e7a21eae1f88301
nevy11 <smongare2004@gmail.com>
2025-09-17 19:02:00 +0300

    create a simple python runner

--- src/app/python-runner.service.ts
+++ src/app/python-runner.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-
 @Injectable({
   providedIn: 'root',
 })
@@ -8,10 +7,18 @@ export class PythonRunnerService {
 
   private async initWasm() {
     if (!this.wasm) {
-      // Import WASM loader from assets
+      // Import the WASM loader from the public folder
+      // const wasmModule = await import('../../public/wasm/wasm_rust');
+
+      // const wasmModule = await import(
+      // new URL('/wasm/wasm_rust.js', window.location.origin).toString()
+
+      // );
+
       const wasmModule = await import(
-        '/media/nevy11/work/projects/ELearningProject/day8/Codevia/public/wasm/wasm_rust.js'
+        /* @vite-ignore */ '../../public/wasm/wasm_rust.js'
       );
+
       this.wasm = await wasmModule.default();
     }
   }
@@ -19,6 +26,11 @@ export class PythonRunnerService {
   // Function to send Python code to Rust and get back a response
   async sendPythonCode(code: string): Promise<string> {
     await this.initWasm();
+
+    if (!this.wasm || !this.wasm.run_python_from_angular) {
+      throw new Error('WASM module not properly initialized');
+    }
+
     return this.wasm.run_python_from_angular(code);
   }
 }
