3ceb0b5d8fefaecb62c4272b695b349ccd55b1b2
nevy11 <smongare2004@gmail.com>
2025-09-17 19:56:46 +0300

    add acorn walk library to the project

--- src/app/layout/learning/code-editor-section/code-editor-section.worker.ts
+++ src/app/layout/learning/code-editor-section/code-editor-section.worker.ts
@@ -1,8 +1,33 @@
 /// <reference lib="webworker" />
 
+import * as acorn from 'acorn';
+import * as walk from 'acorn-walk';
+
 addEventListener('message', ({ data }) => {
   try {
     const result = eval(data);
+    const code = 'let x = 10 + 20;';
+    const ast = acorn.parse(code, { ecmaVersion: 2020 });
+    console.log(ast);
+
+    walk.simple(ast, {
+      VariableDeclaration(node) {
+        console.log('Variable declaration:', node);
+      },
+      BinaryExpression(node) {
+        console.log('Binary expression:', node);
+      },
+    });
+
+    //  finding unsafe
+    walk.simple(ast, {
+      CallExpression(node: any) {
+        if (node.callee.name === 'eval') {
+          console.warn('⚠️ Unsafe code: eval() detected');
+        }
+      },
+    });
+
     postMessage({ success: true, result });
   } catch (error: string | any) {
     postMessage({ success: false, error: error.toString() });
