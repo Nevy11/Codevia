6f799a86a27886d5ca382cb21b1559b6526bd546
nevy11 <smongare2004@gmail.com>
2025-09-19 23:22:59 +0300

    initialized  terminal

---
+++ src/app/layout/learning/code-editor-section/terminal/terminal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nevy11-terminal',
+  imports: [],
+  templateUrl: './terminal.component.html',
+  styleUrl: './terminal.component.scss'
+})
+export class TerminalComponent {
+
+}
