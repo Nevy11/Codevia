b42619a8cb9d83de9cf2e9d550bca53f20f050cb
nevy11 <smongare2004@gmail.com>
2025-11-06 20:59:42 +0300

    remove provideanimations()  tool in alist of providers.. is depreciated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,9 @@ import {
   isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideServiceWorker } from '@angular/service-worker';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideMonacoEditor } from 'ngx-monaco-editor-v2';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
@@ -18,12 +15,10 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideAnimations(),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
     }),
-    provideAnimationsAsync(),
     provideMonacoEditor({
       baseUrl: '/monaco/vs',
       defaultOptions: {
