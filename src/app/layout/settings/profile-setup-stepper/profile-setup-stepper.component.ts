b8b0075231fe0f483ae797ef9240367d860a7278
nevy11 <smongare2004@gmail.com>
2025-12-11 14:01:30 +0300

    remove cards for loop in profile setup stepper

--- src/app/layout/settings/profile-setup-stepper/profile-setup-stepper.component.ts
+++ src/app/layout/settings/profile-setup-stepper/profile-setup-stepper.component.ts
@@ -2,12 +2,11 @@ import { Component, inject } from '@angular/core';
 import { MobileStepperComponent } from './mobile-stepper/mobile-stepper.component';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { map, shareReplay } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
 import { NonMobileStepperComponent } from './non-mobile-stepper/non-mobile-stepper.component';
 
 @Component({
   selector: 'nevy11-profile-setup-stepper',
-  imports: [MobileStepperComponent, AsyncPipe, NonMobileStepperComponent],
+  imports: [MobileStepperComponent, NonMobileStepperComponent],
   templateUrl: './profile-setup-stepper.component.html',
   styleUrl: './profile-setup-stepper.component.scss',
 })
@@ -16,47 +15,5 @@ export class ProfileSetupStepperComponent {
   Ismobile$ = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map((result) => result.matches),
     shareReplay(1)
-  );
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          {
-            mobile_device: true,
-          },
-        ];
-      } else {
-        // tablet potrait view
-        const isTabletPotrait = this.breakpointObserver.isMatched(
-          Breakpoints.TabletPortrait
-        );
-        if (isTabletPotrait) {
-          return [
-            {
-              mobile_device: false,
-            },
-          ];
-        } else {
-          // small laptop view
-          const isSmallLaptop = this.breakpointObserver.isMatched(
-            '(min-width: 840px) and (max-width: 1366px)'
-          );
-          if (isSmallLaptop) {
-            return [
-              {
-                mobile_device: false,
-              },
-            ];
-          } else {
-            return [
-              {
-                mobile_device: false,
-              },
-            ];
-            // Large laptop view
-          }
-        }
-      }
-    })
   );
 }
