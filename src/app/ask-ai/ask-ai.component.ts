4eb7c91a28c261b2c8f78cdfaf802e0d68f08849
Nevy11 <smongare2004@gmail.com>
2026-02-18 16:13:04 +0300

    update ask ai

--- src/app/ask-ai/ask-ai.component.ts
+++ src/app/ask-ai/ask-ai.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'nevy11-ask-ai',
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule,
+     MatButtonModule, MatFormFieldModule,
+    MatInputModule, ReactiveFormsModule],
   templateUrl: './ask-ai.component.html',
   styleUrl: './ask-ai.component.scss',
 })
 export class AskAiComponent {
   open = false;
+  ai_data = signal(
+    {
+      query: new FormControl("")
+    }
+  )
 
   toggleChat() {
     this.open = !this.open;
